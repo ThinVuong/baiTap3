555327ba304d4e97c36ac27a5681814613e273d2
thin vuong <vuongvithin@gmail.com>
2023-03-28 21:09:25 +0700

    change

--- bai_9/Program.cs
+++ bai_9/Program.cs
@@ -25,7 +25,9 @@ namespace bai_9
                     if (NhapSoNguyenDuong(out n, nameof(n)))
                     {
                         Console.WriteLine("<------------------------------->");
-                        Console.WriteLine("Ve tam giac sao(*):\n{0}", Tamgiac(n));
+                        Console.WriteLine("Ve tam giac sao(*) {0} dong (*):\n{1}", nameof(n), Tamgiac(n));
+                        Console.WriteLine("Ve tam giac can sao(*) {0} dong (*):\n{1}", nameof(n), TamgiacCan(n));
+                        Console.WriteLine("Ve tam giac nguoc sao(*) {0} dong (*):\n{1}", nameof(n), TamgiacNguoc(n));
                         Console.WriteLine("<------------------------------->");
                     }
                     else { Console.WriteLine("So ban vua nhap khong dung yeu cau"); }
@@ -49,6 +51,46 @@ namespace bai_9
             {
                 hangSau +="*";
                 hang += "\n" + hangSau;
+            }
+
+            return hang;
+        }
+        static public string TamgiacCan(int x)
+        {
+            string hang = "";
+            string hang1 = "";
+            string hang2 = "";
+            for (int i = 1; i <= x; i++)
+            {
+                for(int j=x-i-1;j>=0;j--)
+                {
+                    hang2 = hang2+" ";
+                }
+                hang1 +="* ";
+                hang +="\n" +hang2+hang1;
+                hang2 = "";
+            }
+
+            return hang;
+        }
+        static public string TamgiacNguoc(int x)
+        {
+            string hang = "";
+            string hang1 = "";
+            string hang2 = "";
+            for (int i = 1; i <= x; i++)
+            {
+                for (int k = 1; k <= i-1; k++)
+                {
+                    hang2 = hang2 + " ";
+                }
+                for (int j = x-i; j >= 0; j--)
+                {
+                    hang1 = hang1 + "* ";
+                }
+                hang += "\n" + hang2 + hang1;
+                hang2 = "";
+                hang1 = "";
             }
 
             return hang;
