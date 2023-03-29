0f3209731b85662f54221c4e6844c0c0e9e56969
thin vuong <vuongvithin@gmail.com>
2023-03-29 15:33:49 +0700

    change

--- bai_9/Program.cs
+++ bai_9/Program.cs
@@ -26,8 +26,11 @@ namespace bai_9
                     {
                         Console.WriteLine("<------------------------------->");
                         Console.WriteLine("Ve tam giac sao(*) {0} dong (*):\n{1}", nameof(n), Tamgiac(n));
+                        Console.WriteLine("Ve tam giac sao(*) {0} 2 dong (*):\n{1}", nameof(n), Tamgiac2(n));
                         Console.WriteLine("Ve tam giac can sao(*) {0} dong (*):\n{1}", nameof(n), TamgiacCan(n));
+                        Console.WriteLine("Ve tam giac can sao(*) 2 {0} dong (*):\n{1}", nameof(n), TamgiacCan2(n));
                         Console.WriteLine("Ve tam giac nguoc sao(*) {0} dong (*):\n{1}", nameof(n), TamgiacNguoc(n));
+                        Console.WriteLine("Ve tam giac nguoc sao(*) 2 {0} dong (*):\n{1}", nameof(n), TamgiacNguoc2(n));
                         Console.WriteLine("<------------------------------->");
                     }
                     else { Console.WriteLine("So ban vua nhap khong dung yeu cau"); }
@@ -51,6 +54,22 @@ namespace bai_9
             {
                 hangSau +="*";
                 hang += "\n" + hangSau;
+            }
+
+            return hang;
+        }
+        static public string Tamgiac2(int x)
+        {
+            string hang = "";
+            string hang1 = "";
+            string hang2 = "";
+            for (int i = 1; i <= x; i++)
+            {
+                hang1 += " ";
+                for (int j = x - i; j > 0; j--)
+                    hang2 += "*";
+                hang += "\n" + hang1 + hang2;
+                hang2 = "";
             }
 
             return hang;
@@ -89,6 +108,50 @@ namespace bai_9
                     hang1 = hang1 + "* ";
                 }
                 hang += "\n" + hang2 + hang1;
+                hang2 = "";
+                hang1 = "";
+            }
+
+            return hang;
+        }
+        static public string TamgiacCan2(int x)
+        {
+            string hang = "";
+            string hang1 = "";
+            string hang2 = "";
+            string hang3 = "";
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = x - i; j >= 1; j--)
+                {
+                    hang2 = hang2 + "*";
+                }
+                hang1 += " ";
+                for(int k=0; k <= i-2; k++)
+                { hang3 += " "; }
+                hang +=(i==1)? "\n" + hang2 + hang1 + hang2 : "\n" + hang2 + hang1 + hang3 +hang2;
+                hang2 = "";
+                hang3 = "";
+            }
+
+            return hang;
+        }
+        static public string TamgiacNguoc2(int x)
+        {
+            string hang = "";
+            string hang1 = "";
+            string hang2 = "";
+            for (int i = 1; i <= x; i++)
+            {
+                for (int k = 1; k <= i - 1; k++)
+                {
+                    hang2 = hang2 + "*";
+                }
+                for (int j = (x-i)*2+1; j >= 1; j--)
+                {
+                    hang1 = hang1 + " ";
+                }
+                hang += "\n" + hang2 + hang1 +hang2;
                 hang2 = "";
                 hang1 = "";
             }
